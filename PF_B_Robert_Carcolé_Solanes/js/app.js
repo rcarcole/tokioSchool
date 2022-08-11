dc7a5f1373cc5490fb3d594fe6ef1e57f7f4c964
rcarcole <rcarcole@hotmail.com>
2022-08-11 19:01:10 +0200

    estilos pagina principal, añadido boton sticky-top, menu bootstrap4

--- PF_B_Robert_Carcolé_Solanes/js/app.js
+++ PF_B_Robert_Carcolé_Solanes/js/app.js
@@ -1,17 +1,25 @@
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
 
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
 
-function sendEmail() {
-    Email.send({
-    Host: "smtp.elasticemail.com",
-    Username : "rcarcoles@gmail.com",
-    Password : "9F06776AB4206FCEF293EFF4C85FA7B271AE",
-    To : 'gsanox@gmail.com',
-    From : "rcarcoles@gmail.com",
-    Subject : "probando",
-    Body : "probandoBody",
-    Port: 2525,
-    }).then(
-        message => alert(message)
-    );
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
