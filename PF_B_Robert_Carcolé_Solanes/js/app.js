75b775146642f92a897949c4aa1dfe0d35db6864
robert <rcarcole@hotmail.com>
2022-08-18 09:20:01 +0200

    añadido api google maps

--- PF_B_Robert_Carcolé_Solanes/js/app.js
+++ PF_B_Robert_Carcolé_Solanes/js/app.js
@@ -1,25 +1,78 @@
-//Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+// //Get the button
+// let mybutton = document.getElementById("btn-back-to-top");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () {
+//   scrollFunction();
+// };
 
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+// function scrollFunction() {
+//   if (
+//     document.body.scrollTop > 20 ||
+//     document.documentElement.scrollTop > 20
+//   ) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+// // When the user clicks on the button, scroll to the top of the document
+// mybutton.addEventListener("click", backToTop);
+
+// function backToTop() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
+
+
+// GoogleMaps API
+let map, infoWindow, marker;
+
+function initMap() {
+    let pos = {lat: 40.884181, lng: 0.804911};
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        pos = {
+          lat: 40.884181,
+          lng: 0.804911,
+        };
+
+        
+    const marker = new google.maps.Marker({
+        position: pos,
+        map,
+    });
+    marker.setMap(map);
+    map.setCenter(pos);
+    },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+    
+  map = new google.maps.Map(document.getElementById("containerMap"), {
+    center:  pos,
+    zoom: 15,
+  });
+  infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+  });
 }
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
 
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
 }
+
+window.initMap = initMap;
