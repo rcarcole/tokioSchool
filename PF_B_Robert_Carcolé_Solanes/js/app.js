9d17b5562970d7be4e00e3d4d9e833ad557f0c7d
rcarcole <rcarcole@hotmail.com>
2022-08-23 16:27:28 +0200

    añadidas imagenes nuevas en especialidades, mejorado css responsive, adaptado el min-height

--- PF_B_Robert_Carcolé_Solanes/js/app.js
+++ PF_B_Robert_Carcolé_Solanes/js/app.js
@@ -1,36 +1,46 @@
-// //Get the button
-// let mybutton = document.getElementById("btn-back-to-top");
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
 
-// // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function () {
-//   scrollFunction();
-// };
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
 
-// function scrollFunction() {
-//   if (
-//     document.body.scrollTop > 20 ||
-//     document.documentElement.scrollTop > 20
-//   ) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
-// // When the user clicks on the button, scroll to the top of the document
-// mybutton.addEventListener("click", backToTop);
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
 
-// function backToTop() {
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
-// }
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 
 function botonEnviar(){
-  console.log("hola");
-  let enviar = document.getElementById("enviar");
-  enviar.addEventListener("click", () => {
-    alert("Mensaje enviado");
-  });
+  let mail = document.getElementById("formControlMail").value;
+  let name = document.getElementById("formControlName").value;
+  console.log(mail);
+  console.log(name);
+  // el if no funciona  
+  if (mail || name != null){
+    console.log("mensaje enviado");
+    let enviar = document.getElementById("enviar");
+    enviar.addEventListener("click", () => {
+      // s'envia varies vegades el alert
+      alert("Mensaje enviado");
+    });
+  } else {
+    alert ("Introduzca un correo electrónico válido y el nombre de reserva.")
+  }
 }
 
 // GoogleMaps API
